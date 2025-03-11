ca4e3ac56976692140f4b37683752e054818962c
Luis Angel Alba Malagon <lalba10@icloud.com>
2025-03-11 20:21:43 +0000

    Avance proyecto 110325

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 export const Footer = () => (
 	<footer className="footer mt-auto py-3 text-center">
 		<p>
-			Check the <a target="_blank" href="https://4geeks.com/docs/start/start-react-advanced-project">template documentation</a> <i class="fa-solid fa-file"></i> for help.
+			Check the <a target="_blank" href="https://playground.4geeks.com/contact/docs?_gl=1*vd0kkk*_gcl_au*MTY4OTE1MDk0Ni4xNzM4ODA2MjA5">template documentation</a> <i className="fa-solid fa-file"></i> for help.
 		</p>
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <i className="fa-solid fa-file-code" /> by{" "}
+			<a href="https://www.linkedin.com/in/luisalba10">Luis Alba</a>👷🚨
 		</p>
 	</footer>
 );
--- src/pages/CreateNewContact.jsx
+++ src/pages/CreateNewContact.jsx
@@ -6,40 +6,69 @@ const CreateNewContact = () => {
     const [phone, setPhone] = useState("");
     const [email, setEmail] = useState("");
     const [address, setAddress] = useState("");
+    const handleRegisterContact = async () => {
+        const newContact = {
+            "name": name,
+            "phone": phone,
+            "email": email,
+            "address": address
+        }
+        try {
+            const response = await fetch("https://playground.4geeks.com/contact/agendas/Lalba10col/contacts", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(newContact)
+            })
+            if (!response.ok) {
+                throw new Error("No se puede crear el Contacto");
+            }
+
+            setName("");
+            setPhone("");
+            setEmail("");
+            setAddress("");
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
 
     return (
         <div>
             <form>
                 <div className="mb-3">
                     <label htmlFor="exampleInputName" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="exampleInputName" onChange={(event) => {
+                    <input type="text" className="form-control" id="exampleInputName" value={name} onChange={(event) => {
                         setName(event.target.value)
                     }} />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="exampleInputPhone" className="form-label">Phone</label>
-                    <input type="text" className="form-control" id="exampleInputPhone" onChange={(event) => {
+                    <input type="text" className="form-control" id="exampleInputPhone" value={phone} onChange={(event) => {
                         setPhone(event.target.value)
                     }} />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" onChange={(event) => {
+                    <input type="email" className="form-control" id="exampleInputEmail1" value={email} onChange={(event) => {
                         setEmail(event.target.value)
                     }} />
                 </div>
 
                 <div className="mb-3">
                     <label htmlFor="exampleInputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="exampleInputAddress" onChange={(event) => {
+                    <input type="text" className="form-control" id="exampleInputAddress" value={address} onChange={(event) => {
                         setAddress(event.target.value)
                     }} />
 
                 </div>
 
-                <button type="button" className="btn btn-primary">Register Contact</button>
+                <button type="button" className="btn btn-primary" onClick={handleRegisterContact}>Register Contact</button>
             </form>
 
         </div>
